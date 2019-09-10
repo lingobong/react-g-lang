ff3966bb6c22b36fe477f4d3444ae42b7a94f51f
ryujaewan <jw.ryu@plingo.io>
2019-09-10 16:38:27 +0900

    packageupdate

--- gl.js
+++ gl.js
@@ -1,18 +1,19 @@
 const React = require('react')
 const GL = require('g-lang')
 
-function _LangReloader(Component: React.ReactNode){
+function _LangReloader(Component: React.ReactNode) {
 
     class Reloader extends React.Component {
         state = { __lang: GL.getLanguage() }
-        constructor(p){
-            super(p)
-            this.onChangeLanguage = GL.onChangeLanguage((__lang) => this.setState({ __lang }))
+        UNSAFE_componentWillMount() {
+            this.onChangeLanguage = GL.onChangeLanguage((__lang) => {
+                this.setState({ __lang })
+            })
         }
-        componentWillUnmount(){
+        componentWillUnmount() {
             this.onChangeLanguage.remove()
         }
-        render ( ) {
+        render() {
             return (<Component {...this.props} __lang={this.state.__lang} />)
         }
     }
@@ -24,7 +25,7 @@ export default _LangReloader
 export const LangReloader = _LangReloader
 export const lang = GL.lang
 
-type ReturnArg<T> = ()=>T
-export const createObject = function<T> ( fn: ReturnArg<T> = () => {  } ): T {
+type ReturnArg<T> = () => T
+export const createObject = function <T>(fn: ReturnArg<T> = () => { }): T {
     return GL.createObject(fn)
 }
